5947c4044e427efd634d2eafccd57c70d776d940
basofiibnu <>
2022-03-24 09:38:05 +0700

    setup tweet box

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
-import Feed from "../components/Feed";
-import Sidebar from "../components/Sidebar";
-import Widgets from "../components/Widgets";
+import Head from 'next/head';
+import Image from 'next/image';
+import Feed from '../components/Home/Feed';
+import Sidebar from '../components/Sidebar';
+import Widgets from '../components/Widgets';
 
 const style = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
