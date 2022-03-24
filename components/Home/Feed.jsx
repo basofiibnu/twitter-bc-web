ce8d21612cd69593bc8f10c2dc3fa2197c08f6ca
basofiibnu <>
2022-03-24 10:17:39 +0700

    setup tweet content and widgets

--- components/Home/Feed.jsx
+++ components/Home/Feed.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsStars } from 'react-icons/bs';
+import Post from '../Post';
 import TweetBox from './TweetBox';
 
 const style = {
@@ -7,6 +8,45 @@ const style = {
   header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
   headerTitle: `text-xl font-bold`,
 };
+
+const tweets = [
+  {
+    displayName: 'Qazi',
+    username: 'loremloremlorem',
+    avatar:
+      'https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2020-06-01T12:00:00.000Z',
+  },
+  {
+    displayName: 'Qazi',
+    username: 'loremloremlorem',
+    avatar:
+      'https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2020-06-01T12:00:00.000Z',
+  },
+  {
+    displayName: 'Qazi',
+    username: 'loremloremlorem',
+    avatar:
+      'https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg',
+    text: 'gm',
+    isProfileImageNft: true,
+    timestamp: '2020-06-01T12:00:00.000Z',
+  },
+  {
+    displayName: 'Qazi',
+    username: 'loremloremlorem',
+    avatar:
+      'https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2020-06-01T12:00:00.000Z',
+  },
+];
 
 const Feed = () => {
   return (
@@ -16,6 +56,19 @@ const Feed = () => {
         <BsStars />
       </div>
       <TweetBox />
+      {tweets.map((tweet, index) => (
+        <Post
+          key={index}
+          displayName={tweet.displayName}
+          userName={`${tweet.username.slice(0, 3)}...${
+            tweet.username - 3
+          }`}
+          avatar={tweet.avatar}
+          text={tweet.text}
+          isProfileImageNft={tweet.isProfileImageNft}
+          timestamp={tweet.timestamp}
+        />
+      ))}
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Widgets from '../components/Widgets';
 
 const style = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
-  content: `max-w-[1400px] w-2/3 flex justify-between`,
+  content: `max-w-[1400px] w-3/4 mx-auto flex justify-between`,
 };
 
 export default function Home() {
